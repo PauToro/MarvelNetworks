b612ae9142fcc1a999e03ab42b46440476a3da2d
Pau Toro <pautoroisaza@gmail.com>
2016-03-16 11:25:20 -0400

    Scrapper Function Definition file and test

---
+++ MarvelWikiScrapper/scrapperDef.py
@@ -0,0 +1,36 @@
+#Scrap Real Name, Alias, and First Appearance from Marvel Wiki page
+
+from bs4 import BeautifulSoup
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+def find_between( aString, first, last):
+	try:
+		start = aString.index(first) + len(first)
+		end = aString.index(last, start)
+		return aString[start:end]
+	except ValueError:
+		return ""
+ 
+def scrapWiki(wikiURL):
+	soup = BeautifulSoup(urlopen(wikiURL))
+	lines = soup.find_all('p')
+	list = []
+	for line in lines:
+		list.append(line.encode('utf-8'))
+	for i in list:
+		if "Real Name" in i:
+			realNameLine = i
+		elif "Aliases" in i:
+			aliasesLine = i
+		elif "First Appearance" in i:
+			firstAppLine = i
+	realName = find_between(realNameLine, "<br/>", "</p>")
+	realName = realName.rstrip("\n")
+	aliases = find_between(aliasesLine, "<br/>", "</p>")
+	aliases = aliases.rstrip("\n")
+	firstApp = find_between(firstAppLine, "<br/>", "</p>")
+	firstApp = firstApp.rstrip("\n")
+	return (realName, aliases, firstApp)
