fa7906630fabd8b47aca41ab3a006ea3baf113d4
Paulina Toro <ptoro@localhost.(none)>
2016-03-30 11:58:21 -0400

    Programs for creating and analyzing network graphs.

---
+++ databases/analyzeHeroNetwork.py
@@ -0,0 +1,31 @@
+#Create network using networkMatch table.
+#Get relevant measures for particular node (character)
+#Draw graph and highlight location of particular node (character)
+
+import networkx as nx
+#import matplotlib.pyplot as plt
+
+G = nx.read_edgelist('networkMatch.csv', delimiter=';')
+
+character = "MAGNETO"
+
+print "Degrees: " + str(G.degree(character))
+close_cen = nx.closeness_centrality(G)
+bet_cen = nx.betweenness_centrality(G)
+
+for name, cc in close_cen.iteritems():
+	if name == character:
+		print "Betweenness Centrality: " + str(cc)
+
+for name, bc in bet_cen.iteritems():
+	if name == character:
+		print "Closeness Centrality: " + str(bc)
+		
+#nx.draw(G)
+#plt.savefig("test.png")
+				
+#cent_items = [(b,a) for (a,b) in close_cen.iteritems()]
+#cent_items.sort()
+#cent_items.reverse()
+
+#print tuple(reversed(cent_items[0]))
---
+++ meeting.py
@@ -0,0 +1,61 @@
+#Two Character Meeting Screen text mock up
+#List of shared appearances, chronological
+#Series with most shared appearances
+#Images of two characters
+
+import sqlite3
+
+from bs4 import BeautifulSoup
+from urllib2 import HTTPError
+try:
+    from urllib2 import urlopen, HTTPError
+except ImportError:
+    from urllib.request import urlopen
+
+def find_between(aString, first, last):
+	try:
+		start = aString.index(first) + len(first)
+		end = aString.index(last, start)
+		return aString[start:end]
+	except ValueError:
+		return ""
+
+def scrapIssue(issueURL):
+	try:		
+		soup = BeautifulSoup(urlopen(issueURL))
+		lines = soup.find_all('a')
+		months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+		i = 0
+		for line in lines:
+			i += 1
+			for month in months:
+				if month in line:
+					lineYear = lines[i].encode('utf-8')
+					global year
+					year = find_between(lineYear, ">", "<")
+		return year
+	except HTTPError:
+		return "No file found."
+
+char1 = raw_input("Character 1: ")
+char2 = raw_input("Character 2: ")
+
+conn = sqlite3.connect('databases/MarvelNetworks')
+
+cursor = conn.execute("SELECT DISTINCT iFN.FullName FROM heroes_in_comics AS hic1 INNER JOIN nameMatch AS nm1 ON hic1.Name = nm1.HICname INNER JOIN heroes_in_comics AS hic2 ON hic1.Issue = hic2.Issue INNER JOIN nameMatch AS nm2 ON hic2.Name = nm2.HICname INNER JOIN IssuesFullNames AS iFN ON hic1.Issue = iFN.FullKey WHERE nm1.APIname = ? AND nm2.APIname = ?" , (char1, char2))
+
+issues = []
+
+for row in cursor:
+	issue = row[0]
+	issueURL = issue.replace(".", "")
+	issueURL = issue.replace("#", "")
+	issueURL = issueURL.replace(" ", "_")
+	issueURL = issueURL.title()
+	issueURL = "http://marvel.wikia.com/wiki/" + issueURL
+	issueYear = scrapIssue(issueURL)
+	issueTuple = (issue, issueYear)
+	print issue
+	print issueYear
+	
+conn.close()
